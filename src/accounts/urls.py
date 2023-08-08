0c089ece9a2537eea1d190b44ff6f101e58e1b94
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2023-08-08 19:47:33 +0400

    login/register templates

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 
+from . import views
+
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
+    # path("accounts/", include("django.contrib.auth.urls")),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
